1fb49c7479d5dcd080bd15ce6299da9dbf6aa915
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-06-11 10:22:29 +0800

    fix bug

--- uglyrag/refiner/base.py
+++ uglyrag/refiner/base.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import List
 
 
-class ReFiner(ABC):
+class Refiner(ABC):
     @abstractmethod
     def __call__(self, query: str, contexts: List[str]) -> List[str]:
         pass
