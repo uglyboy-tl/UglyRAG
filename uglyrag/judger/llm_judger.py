97a665ea1fe5755c20a0615ef7d3607a4c958d17
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-06-12 13:33:06 +0800

    调整使用 llm 能力的 judger 模块和 abstract 模块

---
+++ uglyrag/judger/llm_judger.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from loguru import logger
+
+from uglyrag.generator import Generator, OpenAI_Generator
+
+from .base import Judger
+
+PROMPT_TEMPLATE = """
+I will provide a question. Please determine if this question can be directly answered by the model or if additional information needs to be retrieved to help answer it. Respond with either 'Yes' or 'No' only, without any additional information.
+======
+## Question:
+{question}
+
+## Answer:
+"""
+
+
+@dataclass
+class LLM_Judger(Judger):
+    generator: Generator = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.generator = OpenAI_Generator(PROMPT_TEMPLATE)
+
+    def __call__(self, query: str) -> Optional[str]:
+        result = self.generator(query)
+        logger.debug(f"Judger result: {result}")
+        if result.lower() == "yes":
+            return None
+        return [self.generator(query)]
--- uglyrag/refiner/abstract.py
+++ uglyrag/refiner/abstract.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import List
 
-from uglyrag.generator import BasePromptTemplate, Generator, OpenAI_Generator
+from uglyrag.generator import Generator, OpenAI_Generator
 
 from .base import Refiner
 
@@ -19,11 +19,11 @@ Given the question and the document, summarize the document.
 
 
 @dataclass
-class AbstractiveRecompRefiner(Refiner):
+class AbstractRefiner(Refiner):
     generator: Generator = field(init=False)
 
     def __post_init__(self) -> None:
-        self.generator = OpenAI_Generator(prompt_template=BasePromptTemplate(PROMPT_TEMPLATE))
+        self.generator = OpenAI_Generator(PROMPT_TEMPLATE)
 
     def __call__(self, query: str, contexts: List[str]) -> List[str]:
         return [self.generator(query, contexts)]
