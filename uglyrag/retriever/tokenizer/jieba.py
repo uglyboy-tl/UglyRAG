a07f4fd98b023f843bcfe11f14876643b595ac43
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-06-12 14:49:20 +0800

    优化分词模块

--- uglyrag/retriever/tokenizer/jieba.py
+++ uglyrag/retriever/tokenizer/jieba.py
@@ -1,10 +1,29 @@
+import string
 from dataclasses import dataclass, field
 from typing import List, Optional, Set
 
-import jieba_fast as jieba
+import jieba_fast.posseg as pseg
 
 from .base import BaseTokenizer
 from .stop_words import stop_words
+
+allow_speech_tags = {
+    "an",
+    "i",
+    "j",
+    "l",
+    "n",
+    "nr",
+    "nrfg",
+    "ns",
+    "nt",
+    "nz",
+    "t",
+    "v",
+    "vd",
+    "vn",
+    "eng",
+}
 
 
 @dataclass
@@ -12,7 +31,15 @@ class Jieba(BaseTokenizer):
     stopwords: Optional[Set[str]] = field(default_factory=lambda: set(stop_words))
 
     def __call__(self, text: str) -> List[str]:
-        words = jieba.cut_for_search(text)
-        if self.stopwords:
-            words = [w for w in words if w not in self.stopwords and w.strip()]
+        # 结巴分词
+        jieba_result = pseg.cut(text)
+        # 词性筛选
+        jieba_result = [w for w in jieba_result if w.flag in allow_speech_tags]
+        # 去除特殊符号
+        words = [w.word.strip() for w in jieba_result if w.flag != "x"]
+        # 去除停用词
+        words = [word for word in words if word not in stop_words and word not in string.punctuation and len(word) > 1]
+        # 英文
+        words = [word.lower() for word in words]
+
         return words
