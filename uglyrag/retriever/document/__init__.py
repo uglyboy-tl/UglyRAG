8aff68475c774e995b72e740e589949fd0c664d8
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-06-14 11:10:58 +0800

    修改了 Document 结构的初始化方式，让其更加简单了

--- uglyrag/retriever/document/__init__.py
+++ uglyrag/retriever/document/__init__.py
@@ -3,12 +3,12 @@ from .default import DefaultDocument
 from .file import FileDocument
 
 
-def get_document_class(name: str) -> BaseDocument:
+def get_document(type: str, doc: str) -> BaseDocument:
     # 获取所有当前模块中的类
     subclasses = BaseDocument.__subclasses__()
     for subclass in subclasses:
-        if subclass._name_ == name:
-            return subclass
+        if subclass._name_ == type:
+            return subclass(doc)
     return None
 
 
@@ -16,5 +16,5 @@ __all__ = [
     "BaseDocument",
     "DefaultDocument",
     "FileDocument",
-    "get_document_class",
+    "get_document",
 ]
