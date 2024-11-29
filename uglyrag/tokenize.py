4b7b6957aa7f0c470da717cb81696b4eea5e055c
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-11-29 15:39:29 +0800

    📝  feat(uglyrag): 添加停用词列表和分词功能
    在 `pyproject.toml` 中添加了 `sqlite-vec` 和 `jieba-fast` 依赖，以支持文本处理功能。
    新增 `_stop_words.py` 文件，包含一个包含 1398 个停用词的集合。
    新增 `tokenize.py` 文件，实现了一个 `tokenize` 函数，该函数使用结巴分词对输入文本进行分词，并过滤掉停用词、特殊符号以及长度小于2的词。同时，将英文单词转换为小写。

---
+++ uglyrag/tokenize.py
@@ -0,0 +1,39 @@
+import string
+from typing import List
+
+import jieba_fast.posseg as pseg
+
+from ._stop_words import stop_words
+
+allow_speech_tags = {
+    "an",
+    "i",
+    "j",
+    "l",
+    "n",
+    "nr",
+    "nrfg",
+    "ns",
+    "nt",
+    "nz",
+    "t",
+    "v",
+    "vd",
+    "vn",
+    "eng",
+}
+
+
+def tokenize(text: str) -> List[str]:
+    # 结巴分词
+    jieba_result = pseg.cut(text)
+    # 词性筛选
+    jieba_result = [w for w in jieba_result if w.flag in allow_speech_tags]
+    # 去除特殊符号
+    words = [w.word.strip() for w in jieba_result if w.flag != "x"]
+    # 去除停用词
+    words = [word for word in words if word not in stop_words and word not in string.punctuation and len(word) > 1]
+    # 英文
+    words = [word.lower() for word in words]
+
+    return words
