2fe67eb232f3aee90e708bd1271100ee9f727c93
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-12-06 14:35:46 +0800

    ♻️ refactor(jina): 重构 Jina API 集成
    - 添加日志记录和错误处理，提高代码的健壮性和可维护性
    - 优化请求函数，使其更通用且能够处理异常情况
    - 改进类型注解，增强代码的可读性和安全性
    - 移除冗余的全局变量，简化代码结构

--- src/uglyrag/_integrations/jina.py
+++ src/uglyrag/_integrations/jina.py
@@ -1,3 +1,6 @@
+import logging
+from typing import List
+
 import requests
 
 from uglyrag._config import Config
@@ -6,14 +9,6 @@ config = Config()
 api_key = config.get("api_key", "JINA")
 if api_key is None:
     raise ValueError("API 密钥未设置, 请修改 config.ini 文件，在 [JINA] 中设置 api_key=你的API密钥")
-
-data = {
-    "model": "jina-embeddings-v3",
-    "task": "text-matching",
-    "late_chunking": False,
-    "dimensions": 1024,
-    "embedding_type": "float",
-}
 
 
 class JinaAPI:
@@ -24,11 +19,25 @@ class JinaAPI:
     }
 
     @classmethod
-    def _request(cls, module: str, data: dict):
-        return requests.post(f"{cls.url}/{module}", headers=cls.headers, json=data)
+    def _request(cls, module: str, data: dict, full_url: str = None):
+        if not data:
+            logging.warning("Data is empty, skipping request.")
+            return None
+
+        try:
+            if full_url is None:
+                full_url = f"{cls.url}/{module}"
+            logging.debug(f"Sending POST request to {full_url} with data: {data}")
+            response = requests.post(full_url, headers=cls.headers, json=data)
+            response.raise_for_status()  # 检查响应状态码
+            logging.debug(f"Received response with status code: {response.status_code}")
+            return response.json()
+        except requests.RequestException as e:
+            logging.error(f"Request failed: {e}")
+            return None
 
     @classmethod
-    def embeddings(cls, texts: list):
+    def embeddings(cls, texts: List[str]) -> List[List[float]]:
         data = {
             "model": "jina-embeddings-v3",
             "task": "text-matching",
@@ -38,17 +47,14 @@ class JinaAPI:
         }
         data["input"] = texts
         res = cls._request("embeddings", data)
-        return [object["embedding"] for object in res.json()["data"]]
+        return [object["embedding"] for object in res["data"]]
 
     @classmethod
     def embedding(cls, text: str):
         return cls.embeddings([text])[0]
 
     @classmethod
-    def rerank(cls, query: str, documents: list, top_n: int):
-        data["model"] = "jina-reranker-v2-base-multilingual"
-        data["query"] = query
-        data["docs"] = documents
-        data["top_n"] = top_n
+    def rerank(cls, query: str, documents: List[str], top_n: int):
+        data = {"model": "jina-reranker-v2-base-multilingual", "query": query, "docs": documents, "top_n": top_n}
         res = cls._request("rerank", data)
-        return res.json()["results"]
+        return res["results"]
